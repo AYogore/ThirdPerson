1f5503c26aee4abb0723e671364cd8e76b3176dd
AYogore <AYogore@users.noreply.github.com>
2020-09-24 01:49:09 -0500

    Basic with AddForce

---
+++ 3PController/Assets/Scripts/RigidBodyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10.0f;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+        rigidbody.AddForce(inputDirection * accelerationForce);
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+    }
+
+}
