422d5474765846222dfb321027f9a8159fc5dabe
AYogore <AYogore@users.noreply.github.com>
2020-09-30 22:26:31 -0500

    Camera Player Rotation Movement

--- 3PController/Assets/Scripts/RigidBodyController.cs
+++ 3PController/Assets/Scripts/RigidBodyController.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class RigidBodyController : MonoBehaviour
 {
     [SerializeField]
     private float accelerationForce = 10.0f;
+
     [SerializeField]
     private float maxSpeed = 0.5f;
+
     [SerializeField]
     private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
+
+    [SerializeField]
+    [Tooltip ("How fast player turns. 0 = no turning, 1 = snapping/instant turning")]
+    [Range(0,1)]
+    private float turnSpeed = 0.1f;
 
     private new Rigidbody rigidbody;
     private Vector2 input;
     private new Collider collider;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -43,6 +52,13 @@ public class RigidBodyController : MonoBehaviour
         {
             rigidbody.AddForce(cameraRelativeInputDirection * accelerationForce, ForceMode.Acceleration);
         }
+
+        if(cameraRelativeInputDirection.magnitude > 0) //lets player stay facing direction on camera
+        {
+            var targetRotation = Quaternion.LookRotation(cameraRelativeInputDirection); //change rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
+        }
+        
     }
 
     private void Update()
